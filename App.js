524432f40ecd5711baf8c3a34a35a162e0bb9215
Mart <ae_dts@protonmail.com>
2022-05-10 21:18:02 +0200

    remove debug colors from ChatTab

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -7,15 +7,16 @@ import SettingsScreen from './Components/SettingsScreen';
 import ChatTab from './Components/ChatTab';
 import OpinionScreen from './Components/OpinionScreen';
 import UserChat from './Components/UserChat'
+import React, { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
-function HomeScreen() {
+function HomeScreen({chats}) {
   return (
-    
       <Tab.Navigator>
-        <Tab.Screen name="Gesprekken" component={ChatTab} />
+        <Tab.Screen name="Gesprekken" component={ChatTab} chats={chats} />
         <Tab.Screen name="Meningen" component={OpinionScreen} />
         <Tab.Screen name="Instellingen" component={SettingsScreen} />
       </Tab.Navigator>
@@ -24,10 +25,12 @@ function HomeScreen() {
 }
 
 export default function App() {
+ 
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}}/>
+        <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}} />
         <Stack.Screen name="UserChat" component={UserChat} />
       </Stack.Navigator>
 
--- Components/ChatTab.js
+++ Components/ChatTab.js
@@ -1,21 +1,17 @@
-import { Text, View, FlatList, StyleSheet, Image} from "react-native";
-import AsyncStorage from '@react-native-async-storage/async-storage'
+import { Text, View, FlatList, StyleSheet, Image } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, { useState, useEffect } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
 function Message(time, text, fromSelf) {
-  return {time: time, text: text, fromSelf: fromSelf}
+  return { time: time, text: text, fromSelf: fromSelf };
 }
 
 function User(img, name, userId) {
-   return {img: img, name: name, userId: userId}
+  return { img: img, name: name, userId: userId };
 }
 
 
-
-/*
 const Chats = [
   {
     user: User('./assets/person1.jpg', 'Jolanda', 1),
@@ -32,89 +28,130 @@ const Chats = [
       Message(new Date(), "Lorem ipsum", false),
     ],
     topic: "Rutte"
-  }
+  },
+  
 ]
-*/
+  
+
 
   
 
+
+
 const _storeData = async () => {
   try {
-    await AsyncStorage.setItem(
-      '@Chats',
-      JSON.stringify(Chats)
-    );
-    console.log('Saved data...')
+    await AsyncStorage.setItem('@Chats', JSON.stringify(Chats));
+    console.log('Saved data...');
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 };
-
 
 const _readData = async () => {
   try {
     const data = await AsyncStorage.getItem('@Chats');
-    
   } catch (error) {
     console.error(error);
   }
-}
+};
 
-
-function ListItem({name, lastMessage, nav}) {
+function ListItem({ name, lastMessage, nav }) {
   useEffect(() => {
-    console.log('from listitem: ')
-    console.log(nav)
-  })
-  
+    console.log('from listitem: ');
+    console.log(nav);
+  });
+
   return (
     <View
       style={{
-        flexDirection: "row",
+        flexDirection: 'row',
         height: 90,
-        paddingLeft:15,
+        paddingLeft: 15,
         paddingRight: 15,
       }}
     >
-      <View onTouchEnd={() => nav.navigate('UserChat')} style={{ backgroundColor: "blue", flex: 0.25, borderBottomColor: 'black',borderBottomWidth: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <Image source={require('../assets/person1.jpg')} style={
-          {width: '70%', height: undefined, aspectRatio: 1, borderRadius: 40, }} />
+      <View
+        onTouchEnd={() => nav.navigate('UserChat')}
+        style={{
+          
+          flex: 0.25,
+          borderBottomColor: 'grey',
+          borderBottomWidth: 1,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Image
+          source={require('../assets/person1.jpg')}
+          style={{
+            width: '70%',
+            height: undefined,
+            aspectRatio: 1,
+            borderRadius: 40,
+          }}
+        />
       </View>
-      <View style={{ backgroundColor: "red", flex: 0.75, borderBottomColor: 'black',borderBottomWidth: 1 }} > 
-        <Text style={{paddingBottom: 7, paddingTop: 5, paddingLeft: 5, fontSize: 15, fontWeight: 'bold'}}>{name}</Text>
-        <Text onTouchEnd={_readData} style={{paddingLeft: 5, fontSize: 14, paddingRight: 40, }} numberOfLines={2}>{lastMessage}</Text>
+      <View
+        style={{
+          
+          flex: 0.75,
+          borderBottomColor: 'grey',
+          borderBottomWidth: 1,
+        }}
+      >
+        <Text
+          style={{
+            paddingBottom: 7,
+            paddingTop: 10,
+            paddingLeft: 5,
+            fontSize: 15,
+            fontWeight: 'bold',
+          }}
+        >
+          {name}
+        </Text>
+        <Text
+          onPress={_readData}
+          style={{ paddingLeft: 5, fontSize: 14, paddingRight: 40 }}
+          numberOfLines={2}
+        >
+          {lastMessage}
+        </Text>
       </View>
     </View>
-  )
+  );
 }
 
-const ChatList = ({ chats, nav}) => {
+const ChatList = ({ chats, nav }) => {
   useEffect(() => {
-    console.log("from chatlist:")
-    console.log(nav)
-    
-  })
-  
+    console.log('from chatlist:');
+    console.log(nav);
+  });
 
   const renderItem = ({ item }) => (
-    <ListItem name={item.user.name} lastMessage={item.messages[0].text} nav={nav} />
+    <ListItem
+      name={item.user.name}
+      lastMessage={item.messages[0].text}
+      nav={nav}
+    />
   );
-
 
   return (
-      <FlatList
-        style={styles.list}
-        data={chats}
-        renderItem={renderItem}
-        keyExtractor={item => item.user.userId}
-        nav={nav}
-        
-      />
+    <FlatList
+      style={styles.list}
+      data={chats}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.user.userId}
+      nav={nav}
+    />
   );
-}
+};
 
-function ChatTab({navigation}) {
+function ChatTab({ navigation }) {
   const [chats, setChats] = useState();
+  useEffect(() => {_storeData()})
+  
 
   const readChats = async () => {
     try {
@@ -124,31 +161,29 @@ function ChatTab({navigation}) {
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   // Empty dependency array to prevent from firing infinitely
   useEffect(async () => {
     readChats();
-    console.log(navigation)
-  }, [])
+    console.log(navigation);
+  }, []);
 
-  return (
-    <ChatList chats={chats}  nav={navigation} />
-  )
+  return <ChatList chats={chats} nav={navigation} />;
 }
 
 const styles = StyleSheet.create({
   list: {
-    borderWidth: 2,
-    borderColor: 'black',
-    backgroundColor: 'pink'
+    backgroundColor: 'white'
+    
+    
   },
   listItem: {
     backgroundColor: 'beige',
     width: '80%',
     marginLeft: 'auto',
-    marginRight: 'auto'
-  }
-})
+    marginRight: 'auto',
+  },
+});
 
 export default ChatTab;
